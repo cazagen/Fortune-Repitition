825856f8e62b1d2f31c7a38938b903f183c4d89d
Cameron Sharp <sharpcameron@hotmail.co.uk>
2017-03-06 17:13:39 +0000

    Create Repeater.java

---
+++ Repeater.java
@@ -0,0 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Repeater {
+        public static void main(String[] args) throws FileNotFoundException{
+                PrintWriter out = new PrintWriter("output");
+                int n = Integer.parseInt(args[0]);
+                if(n>10000){
+                        System.out.println("PLS NO, TOO MANY");
+                        System.exit(0);
+                }else{
+            String word = args[1];
+            String word2 = args[1];
+            //System.out.println(word);
+            //System.out.println('%');
+            out.println(word);
+            out.println('%');
+                for(int i = 0; i<n;i++){
+                        word2 = word2 + word;
+                        //System.out.println(word2);
+                        //System.out.println('%');
+                        out.println(word2);
+                        out.println('%');
+                }
+                out.close();
+                }
+        }
+}
+
+
+
+
+
+
+
+
+
